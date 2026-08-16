f3670c8abd9603b129844623852f105b8b607683
ormando-hacks <tm5007185@gmail.com>
2026-08-16 12:38:03 +0100

    Delete autoChatConstants.js

--- autoChatConstants.js
+++
@@ -1,6 +0,0 @@
-// Sentinel the model returns when it decides NOT to jump into a group
-// conversation on its own (autonomous "control yourself" mode). Chosen to be
-// unlikely to appear in normal conversation.
-module.exports = {
-  AUTO_CHAT_SKIP_TOKEN: '[[NO_REPLY]]'
-};
